e008ff06d81ae811da13d40a91c1e582edf16f17
gbogomilov <104738132+gbogomilov@users.noreply.github.com>
2022-12-08 17:06:31 +0200

    Delete New folder (2) directory

--- New folder (2)/galery.js
+++
@@ -1,9 +0,0 @@
-const pictureCount = 5 + +Math.random().toString()[5];
-for (let i = 0; i <= pictureCount; i++) {
-  document
-    .getElementsByClassName("imgContainer")[0]
-    .insertAdjacentHTML(
-      "afterbegin",
-      `<img src="https://picsum.photos/300?dummy=${Math.random()}" alt="">`
-    );
-}
