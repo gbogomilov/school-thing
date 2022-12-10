893dcb82fd0f7ea91afffa0df9f0209b4d51bae3
gbogomilov <104738132+gbogomilov@users.noreply.github.com>
2022-12-11 01:46:18 +0200

    Add files via upload

--- news.js
+++ news.js
@@ -1,20 +1,23 @@
-const pictureCount = Math.random() * (10 - 5 + 1) + 5;
+const pictureCount = 5 + +Math.random().toString()[5];
 
-const url = 'https://api.newscatcherapi.com/v2/search?q=Tesla';
+const url =
+  "https://newsapi.org/v2/everything?" +
+  "q=Apple&" +
+  "from=2022-11-01&" +
+  "sortBy=popularity&" +
+  "apiKey=c0e01f533816447ba96880af97f1e169";
+
+const req = new Request(url);
 
 const requestNews = async () => {
-  const response = await fetch(url, {
-    headers: { 'x-api-key': 'pdEuD4dHdsKq5q2ah22RwfmjuCoFzvUx2sGppULytJU' },
-    'Content-Type': 'application/json',
-  });
-
+  const response = await fetch(req);
   const { articles } = await response.json();
   for (let i = 0; i <= pictureCount; i++) {
     document
-      .getElementsByClassName('content')[0]
+      .getElementsByClassName("content")[0]
       .insertAdjacentHTML(
-        'afterbegin',
-        `<a class="newsStory"  href="${articles[i].link}"><h1>${articles[i].title}</h1><p>${articles[i].summary}</p><p class="author">${articles[i].author}</p></a>`
+        "afterbegin",
+        `<a class="newsStory"  href="${articles[i].url}"><h1>${articles[i].title}</h1><p>${articles[i].description}</p><p class="author">${articles[i].author}</p></a>`
       );
   }
 };
